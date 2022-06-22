b19c67209ad493bf8faf91014819ffd7db84ff67
AnirudhKumar10 <anirudh3104@gmail.com>
2022-06-22 11:21:53 +0530

    check for bst

--- C++/Check for BST.cpp
+++ C++/Check for BST.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 using namespace std;
-
-int main()
+struct Node
 {
+    int data;
+    Node *left;
+    Node *right;
+};
 
-    cout << "Hello World" << endl;
-    return 0;
-}
+class Solution
+{
+public:
+    // Function to check whether a Binary Tree is BST or not.
+    int buffer = -1, flag = true;
+
+    bool isBST(Node *root)
+    {
+
+        if (!flag)
+            return false;
+
+        if (root->left)
+            isBST(root->left);
+
+        if (buffer != -1 && buffer >= root->data)
+            flag = false;
+        else
+            buffer = root->data;
+
+        if (root->right)
+            isBST(root->right);
+
+        return flag;
+    }
+};
+
+// Input 2 1 3
+// Output 1
+
+// Input 2 2 2 N N N N
+// Output 0
+
+// https://practice.geeksforgeeks.org/problems/check-for-bst/
